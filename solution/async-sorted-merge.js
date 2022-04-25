7e14c145ad43ae11a74eb74f2c2586244aaac045
nicolas <nclszhang92@gmail.com>
2022-04-25 19:13:14 -0400

    implement funtionalities for async, sync sorts

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -2,8 +2,35 @@
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
+const PriorityQueue = require("./priority-queue");
+
+const sortSources = async (logSources, printer) => {
+  let candidates = new PriorityQueue(
+    (a, b) => a.entry.date.getTime() < b.entry.date.getTime()
+  );
+
+  await Promise.all(
+    logSources.map(async (logSource) => {
+      const entry = await logSource.popAsync();
+      if (entry) candidates.push({ source: logSource, entry: entry });
+    })
+  );
+
+  while (candidates.size() > 0) {
+    const bestCandidate = candidates.pop();
+    printer.print(bestCandidate.entry);
+    const newEntry = await bestCandidate.source.popAsync();
+    if (newEntry)
+      candidates.push({ source: bestCandidate.source, entry: newEntry });
+  }
+  printer.done();
+};
+
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    sortSources(logSources, printer)
+      .then(() => resolve(console.log("Async sort complete.")))
+      .catch(() => reject(console.log("Async sort is failed.")));
   });
 };
+
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -2,6 +2,26 @@
 
 // Print all entries, across all of the sources, in chronological order.
 
+const PriorityQueue = require("./priority-queue");
+
 module.exports = (logSources, printer) => {
+  let candidates = new PriorityQueue(
+    (a, b) => a.entry.date.getTime() < b.entry.date.getTime()
+  );
+
+  logSources.forEach((logSource) => {
+    const entry = logSource.pop();
+    if (entry) candidates.push({ source: logSource, entry: entry });
+  });
+
+  while (candidates.size() > 0) {
+    const bestCandidate = candidates.pop();
+    printer.print(bestCandidate.entry);
+    const newEntry = bestCandidate.source.pop();
+    if (newEntry)
+      candidates.push({ source: bestCandidate.source, entry: newEntry });
+  }
+  printer.done();
   return console.log("Sync sort complete.");
 };
+
